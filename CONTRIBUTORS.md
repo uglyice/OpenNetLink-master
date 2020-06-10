5a203036beccd94555b08781cc9adfefbd5acede
uglyice <uglyice@naver.com>
2020-06-10 15:05:06 +0900

    초기 커밋.

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,6 @@
+
+# Contributors
+- [Master](https://github.com/hanssak)
+- [kdy800](https://github.com/kdy800)
+- [battledir](https://github.com/battledir)
+- [silencep97](https://github.com/silencep97)
---
+++ src/OpenNetLinkApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using WebWindows.Blazor;
+using System;
+
+namespace OpenNetLinkApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ComponentsDesktop.Run<Startup>("  SecureGate App", "wwwroot/index.html");
+        }
+    }
+}
